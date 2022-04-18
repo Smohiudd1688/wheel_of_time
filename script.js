c49b2bc801c0712df0ad01f0d6adf3cd69dae6a8
Sana Mohiuddin <smohiuddin1688@gmail.com>
2022-04-17 21:11:19 -0700

    use DOM to add some headers, the update meet character section

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-/*const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-
-document.querySelector("body").appendChild(h2);*/
+const h3 = document.createElement("h3");
+h3.textContent = "Meet the Cast of Characters";
+const characters = document.querySelector("div#characters")
+characters.insertBefore(h3, characters.firstChild);
+//using the dom to add a header for the characters section
